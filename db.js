c636581cc6502ed553be5b0f1b8150dd4774ae95
todddrinkwater <todddrinkwaternz@gmail.com>
2017-05-10 12:32:57 +1200

    create new animal

--- db.js
+++ db.js
@@ -2,7 +2,9 @@
 module.exports = {
   getUser: getUser,
   getUsers: getUsers,
-  getProfiles: getProfiles
+  getProfiles: getProfiles,
+  getAllProfiles: getAllProfiles,
+  addProfile: addProfile,
 }
 
 function getUsers (connection) {
@@ -17,3 +19,41 @@ function getProfiles (id, connection) {
   return connection('profiles').select()
   .where('id', id)
 }
+
+function getAllProfiles (connection) {
+  return connection('profiles').select()
+}
+
+function addProfile (newData,connection) {
+  return connection('profiles')
+    .insert({
+      user_id: newData.user_id,
+      pic: newData.pic,
+      url: newData.url
+    })
+    .then((connection('users')))
+      .insert({
+        id: newData.user_id,
+        email: newData.email,
+        name: newData.name
+      })
+}
+
+
+// function addProf(newData,connection){
+//   return connection('profiles')
+//     .insert({
+//       user_id: newData.user_id,
+//       pic: newData.pic,
+//       url: newData.url
+//     })
+//   }
+//
+// function addDetails(newData,connection){
+//   return connection('users')
+//     .insert({
+//       id: newData.user_id,
+//       email: newData.email,
+//       name: newData.name
+//     })
+// }
--- routes/index.js
+++ routes/index.js
@@ -13,17 +13,50 @@ router.get('/', function (req, res) {
     })
 })
 
+router.get('/profiles', function (req, res) {
+  db.getAllProfiles(req.app.get('connection'))
+    .then(function (users) {
+      res.render('profiles', { profiles: users })
+    })
+    .catch(function (err) {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
 router.get('/profiles/:id', function (req,res) {
   var id = req.params.id
   db.getProfiles(id,req.app.get('connection'))
   .then(function (user) {
-    console.log(user);
     res.render('profiles', {profiles:user })
   })
   .catch(function (err) {
     res.status(500).send('DATABASE ERROR: ' + err.message)
   })
 })
+router.get('/new', function (req,res) {
+db.getUsers(req.app.get('connection'))
+  .then(function (users) {
+    res.render('form', { users: users })
+  })
+  .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+})
+//
+router.post('/new', (req,res) => {
+  var newData = req.body
+  console.log(newData)
+  db.addProfile(newData,req.app.get('connection'))
+  .then(function (users) {
+    var id = newData.user_id
+    res.redirect('/profiles/:id')
+  })
+  .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+})
+
+
 
 
 module.exports = router
